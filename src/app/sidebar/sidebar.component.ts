0db09ddf5ea4b592f10d844b9d0ce5418b92d551
Nile Walker <niwal7@morgan.edu>
2021-02-16 09:07:01 -0500

    Updated Linting

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
   constructor() { }
-  opened: boolean = false;
+  opened = false;
   Toggle(){
     this.opened = !this.opened;
   }
